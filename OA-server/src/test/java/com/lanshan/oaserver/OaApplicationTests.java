e05bb211c2aa0722b98748721974f7154d415e27
朱易轩 <2217073762@qq.com>
2024-05-16 21:18:51 +0800

    Changes2

---
+++ OA-server/src/test/java/com/lanshan/oaserver/OaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanshan.oaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
