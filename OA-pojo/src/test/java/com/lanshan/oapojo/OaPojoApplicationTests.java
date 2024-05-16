49e7d0782e3f1d9cdd064bfd4b3aabf396573fee
朱易轩 <2217073762@qq.com>
2024-05-16 21:35:33 +0800

    Changes2
    Signed-off-by: 朱易轩 <2217073762@qq.com>

---
+++ OA-pojo/src/test/java/com/lanshan/oapojo/OaPojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanshan.oapojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaPojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
