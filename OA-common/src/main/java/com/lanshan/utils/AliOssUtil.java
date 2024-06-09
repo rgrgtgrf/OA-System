07d70fb38c87bfd20b290431e4d7adbfb9ebbd42
朱易轩 <2217073762@qq.com>
2024-06-09 17:11:10 +0800

    Changes3
    Signed-off-by: 朱易轩 <2217073762@qq.com>

--- OA-common/src/main/java/com/lanshan/utils/AliOssUtil.java
+++ OA-common/src/main/java/com/lanshan/utils/AliOssUtil.java
@@ -7,12 +7,14 @@ import com.aliyun.oss.OSSException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 
 import java.io.ByteArrayInputStream;
 
 @Data
 @AllArgsConstructor
 @Slf4j
+@ConditionalOnMissingBean
 public class AliOssUtil {
 
     private String endpoint;
